efe8f9855c950b28b964e21df661e0e9aee72071
Viktoriya Riabets <vikamalik30@gmail.com>
2024-06-13 10:39:26 -0400

    make build

---
+++ index.2a1def23.js
@@ -0,0 +1,2 @@
+window.onload=function(){var t=document.querySelector(".wall"),e=document.querySelector(".spider"),o=t.getBoundingClientRect(),n=o.width,i=o.height,l=e.getBoundingClientRect(),c=l.width,d=l.height;e.style.position="absolute",e.style.top="".concat((i-d)/2,"px"),e.style.left="".concat((n-c)/2,"px")};
+//# sourceMappingURL=index.2a1def23.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,18 @@
+"use strict";
+window.onload = function() {
+    const field = document.querySelector(".wall");
+    const spider = document.querySelector(".spider");
+    const fieldRect = field.getBoundingClientRect();
+    const fieldWidth = fieldRect.width;
+    const fieldHeight = fieldRect.height;
+    const spiderRect = spider.getBoundingClientRect();
+    const spiderWidth = spiderRect.width;
+    const spiderHeight = spiderRect.height;
+    const spiderTop = (fieldHeight - spiderHeight) / 2;
+    const spiderLeft = (fieldWidth - spiderWidth) / 2;
+    spider.style.position = "absolute";
+    spider.style.top = `${spiderTop}px`;
+    spider.style.left = `${spiderLeft}px`;
+};
+
+//# sourceMappingURL=index.f75de5e1.js.map
